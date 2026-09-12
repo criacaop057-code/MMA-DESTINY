a3fc516c449f84df6078892b58d2292105537782
criacaop057-code <criacaop057@gmail.com>
2026-09-12 15:25:36 -0300

    Implement priority system for event sorting

--- js/engine/priority.js
+++ js/engine/priority.js
@@ -1,1 +1,17 @@
-// Priority system
+import { EVENT_PRIORITY } from "../core/constants.js";
+
+export function getPriorityValue(priority) {
+    if (typeof priority === "number") {
+        return priority;
+    }
+
+    return EVENT_PRIORITY[priority] ?? EVENT_PRIORITY.NORMAL;
+}
+
+export function comparePriority(a, b) {
+    return getPriorityValue(b.priority) - getPriorityValue(a.priority);
+}
+
+export function sortEvents(events) {
+    return [...events].sort(comparePriority);
+}
