67c821bdfd1f1c9afa16bb779e4320a31844fef8
criacaop057-code <criacaop057@gmail.com>
2026-09-12 15:21:51 -0300

    Update constants.js

--- js/core/constants.js
+++ js/core/constants.js
@@ -1,1 +1,78 @@
-// Constants
+export const GAME_VERSION = "0.1.0";
+
+export const TABS = {
+    HOME: "inicio",
+    CAREER: "carreira",
+    WORLD: "mundo",
+    LIFE: "vida",
+    DYNASTY: "dinastia"
+};
+
+export const GAME_PHASES = {
+    MENU: "menu",
+    CREATION: "creation",
+    PLAYING: "playing",
+    ERROR: "error"
+};
+
+export const AGE = {
+    MIN_START: 15,
+    MAX: 100
+};
+
+export const WEEK = {
+    DAYS: 7
+};
+
+export const MONTHS = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+];
+
+export const DAYS = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+];
+
+export const EVENT_PRIORITY = {
+    CRITICAL: 100,
+    HIGH: 75,
+    NORMAL: 50,
+    LOW: 25,
+    BACKGROUND: 10
+};
+
+export const DEFAULT_CURRENCY = "BRL";
+
+export const WEIGHT_CLASSES = [
+    "Peso Mosca",
+    "Peso Galo",
+    "Peso Pena",
+    "Peso Leve",
+    "Peso Meio-Médio",
+    "Peso Médio",
+    "Peso Meio-Pesado",
+    "Peso Pesado",
+    "Peso Superpesado"
+];
+
+export const GAME_LIMITS = {
+    MAX_EVENT_QUEUE: 1000,
+    MAX_HISTORY_ENTRIES: 5000,
+    MAX_NEWS_ENTRIES: 5000
+};
