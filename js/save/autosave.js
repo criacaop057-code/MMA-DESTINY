ab1b02d6d287007e4775159aaf744ce14b929b90
criacaop057-code <criacaop057@gmail.com>
2026-09-12 17:40:36 -0300

    Update autosave.js

--- js/save/autosave.js
+++ js/save/autosave.js
@@ -1,1 +1,51 @@
-// Autosave
+import { saveGame } from "./save.js";
+
+let autosaveEnabled = true;
+let autosaveInterval = null;
+
+export function enableAutosave() {
+    autosaveEnabled = true;
+}
+
+export function disableAutosave() {
+    autosaveEnabled = false;
+}
+
+export function isAutosaveEnabled() {
+    return autosaveEnabled;
+}
+
+export function performAutosave(state) {
+    if (!autosaveEnabled) {
+        return {
+            success: false,
+            skipped: true
+        };
+    }
+
+    return saveGame(state);
+}
+
+export function startAutosave(
+    getState,
+    interval = 60000
+) {
+    stopAutosave();
+
+    autosaveInterval = setInterval(() => {
+        const state = getState?.();
+
+        if (state) {
+            performAutosave(state);
+        }
+    }, interval);
+
+    return autosaveInterval;
+}
+
+export function stopAutosave() {
+    if (autosaveInterval) {
+        clearInterval(autosaveInterval);
+        autosaveInterval = null;
+    }
+}
