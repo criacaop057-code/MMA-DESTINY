8e00065a207c84f9ac806af14967e92588da2b49
criacaop057-code <criacaop057@gmail.com>
2026-09-12 15:23:10 -0300

    Update ids.js

--- js/core/ids.js
+++ js/core/ids.js
@@ -1,1 +1,37 @@
-// ID generator
+let counter = 0;
+
+function randomPart() {
+    return Math.random()
+        .toString(36)
+        .substring(2, 8)
+        .toUpperCase();
+}
+
+export function generateId(prefix = "ID") {
+    counter++;
+
+    const timestamp = Date.now().toString(36).toUpperCase();
+
+    return `${prefix}-${timestamp}-${counter}-${randomPart()}`;
+}
+
+export function resetIdCounter() {
+    counter = 0;
+}
+
+export const ID = {
+    fighter: () => generateId("FTR"),
+    event: () => generateId("EVT"),
+    fight: () => generateId("FGT"),
+    organization: () => generateId("ORG"),
+    contract: () => generateId("CTR"),
+    news: () => generateId("NWS"),
+    relationship: () => generateId("REL"),
+    child: () => generateId("CHD"),
+    academy: () => generateId("ACD"),
+    vehicle: () => generateId("CAR"),
+    property: () => generateId("HSE"),
+    transaction: () => generateId("TXN"),
+    training: () => generateId("TRN"),
+    eventQueue: () => generateId("QUE")
+};
